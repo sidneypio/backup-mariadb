9051fb206cb50e91233a0b8a514e78c509cb8719
Sidney Pio de Campos <spcampos@unicamp.br>
2019-08-23 11:29:57 -0300

    alteracoes na estrutura do script permitindo a configuracao por arquivo

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 Script simples para rodar o mysqldump e escrever os arquivos de backup separados em um diretorio
 Apagará os backups antigos (x dias configurados em uma variável dentro do script)
 
-Assim podemos rodar via crontab e nao havera preocupacao de encher todo o disco
+Assim podemos rodar via crontab e nao havera preocupacao de encher todo o disco.
+
+As configuracoes deverao ser realizadas no arquivo config.txt presente no mesmo diretorio do script.
+O arquivo config.txt.sample eh um exemplo do formato desse arquivo.
+
 
--- faz-backup-mysqldump.sh
+++ faz-backup-mysqldump.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 #
 # script simples para fazer mysqldump em um diretorio
-# Exemplos de mysqldump e mysql:
+# Exemplos de utilizacao do mysqldump e mysql:
 # mysqldump -u <usuario> -p<senha> mysql user db func > mysqltables.sql
 # mysql -u <usuario> -p<senha> < mysql.sql
 # sidney - 18/12/2017
 
-
-DIRBACKUP=backup
-DATA=`date +%Y%m%d-%H%M%S`
-MYSQLDUMP=/usr/bin/mysqldump
-DATABASES="mysql "
+source ./config.txt
 
 echo "Executando dump em $DIRBACKUP/$DATA"
 mkdir -p $DIRBACKUP/$DATA
 for d in $DATABASES;
 do
 	echo "Backup de $d"
-	$MYSQLDUMP $d > $DIRBACKUP/$DATA/$d.dump
+	$MYSQLDUMP -u $ADMIN -p$PASS $d > $DIRBACKUP/$DATA/$d.dump
 done
+
+echo "Removendo entradas antigas"
+$FIND $DIRBACKUP -type d -ctime +$DIAS -exec rm -rf {} \; 
